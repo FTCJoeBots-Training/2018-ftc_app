1d3ab394edece89158af7f1c4ff44262a31a8222
10518 <sanjanameresh@gmail.com>
2018-09-29 11:41:49 -0400

    Sanjan: Motors 1 and 2.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleOp2017JoeBot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleOp2017JoeBot.java
@@ -26,7 +26,7 @@ import com.qualcomm.robotcore.hardware.DcMotor;
  * List of issues at Comp(1)-> https://docs.google.com/a/stjoebears.com/spreadsheets/d/1r_liipKBU7GHfONdxq9E6d4f7zikcCuXwDL2bsQfwm0/edit?usp=sharing
  *G-Sheet of time VS Heading for autonomous -> https://docs.google.com/a/stjoebears.com/spreadsheets/d/1pqv0iN94fFd5KvX1YIWP7z39HgpURXsscn0zPujs1q4/edit?usp=sharing
 */
-@TeleOp(name="Twins TeleOp", group="TeleOp")
+@TeleOp(name="San Mecanum", group="TeleOp")
 
 public class teleOp2017JoeBot extends LinearOpMode {
 
